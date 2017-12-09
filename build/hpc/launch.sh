89eb374511736a6b4212e50358ee64ad94eaefbf
Christoph Honal <christoph.honal@web.de>
2017-12-09 13:46:09 +0100

    fix launch, again

--- build/hpc/launch.sh
+++ build/hpc/launch.sh
@@ -11,7 +11,8 @@ mkdir -p $DATADIR
 echo Loading netcdf module
 module load netcdf
 
-echo Running with srun...
+echo You are on $(hostname)
+echo Running with srun on $(srun hostname)...
 
 srun $INPUTDIR/SWE_intel_release_none_fwave --grid-size-x=1000 --grid-size-y=1000 --input-bathymetry=$INPUTDIR/input/tohoku_2011/tohoku_gebco_ucsb3_2000m_hawaii_bath.nc --input-displacement=$INPUTDIR/input/tohoku_2011/tohoku_gebco_ucsb3_2000m_hawaii_displ.nc --time-duration=30000 --checkpoint-amount=1000 --boundary-condition-left=0 --boundary-condition-right=0 --boundary-condition-top=0 --boundary-condition-bottom=0 --output-basepath=$DATADIR/swe_tohoku > $DATADIR/stdout.log 2>$DATADIR/stderr.log
 
