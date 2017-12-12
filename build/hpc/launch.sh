0da6f98dfdf5e6f5bea55c76576432ac129c62f1
LRZ-SIM Funktionskennung <di57yec@mpp2-login6.cos.lrz.de>
2017-12-12 16:26:49 +0100

    adjust launch config

--- build/hpc/launch.sh
+++ build/hpc/launch.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-INPUTDIR=$HOME/SWE/build
-DATADIR=$SCRATCH/$USER/swe_data
+INPUTDIR="$HOME/SWE/build"
+DATADIR="$SCRATCH/$USER/swe_data"
 echo INPUTDIR: $INPUTDIR
 echo DATADIR: $DATADIR
 
 MODE=release
 SIZE=1000
-BATHY=$INPUTDIR/input/tohoku_2011/tohoku_gebco_ucsb3_2000m_hawaii_bath.nc
-DISPL=$INPUTDIR/input/tohoku_2011/tohoku_gebco_ucsb3_2000m_hawaii_displ.nc
+BATHY="$INPUTDIR/input/tohoku_2011/tohoku_gebco_ucsb3_2000m_hawaii_bath.nc"
+DISPL="$INPUTDIR/input/tohoku_2011/tohoku_gebco_ucsb3_2000m_hawaii_displ.nc"
 TIME=30000
 CHECKP=1000
 OUTPNAME=tohoku_2011
@@ -17,13 +17,18 @@ TSNOW=$(date +%s)
 echo Current timestamp: $TSNOW
 
 echo You are on $(hostname)
-echo Running with srun on $(srun hostname)...
+echo Running with srun on $(srun hostname)
 
 run_swe () {
-	MYDATADIR=$DATADIR/$TSNOW/$1/$OUTPNAME
+	MYDATADIR="$DATADIR/$TSNOW/$1/$OUTPNAME"
 	echo Using $MYDATADIR
 	mkdir -p $MYDATADIR
-	srun $INPUTDIR/SWE_$1_$MODE_none_fwave --grid-size-x=$SIZE --grid-size-y=$SIZE --input-bathymetry=$BATHY --input-displacement=$DISPL --time-duration=$TIME --checkpoint-amount=$CHECKP --boundary-condition-left=0 --boundary-condition-right=0 --boundary-condition-top=0 --boundary-condition-bottom=0 --output-basepath=$MYDATADIR/swe --output-scale=1 > $MYDATADIR/stdout.log 2>$MYDATADIR/stderr.log
+	RCMD="$INPUTDIR/SWE_$1_$MODE"
+	RCMD+="_none_fwave"
+	RCMD+=" --grid-size-x=$SIZE --grid-size-y=$SIZE --input-bathymetry=$BATHY --input-displacement=$DISPL --time-duration=$TIME --checkpoint-amount=$CHECKP"
+	RCMD+=" --boundary-condition-left=0 --boundary-condition-right=0 --boundary-condition-top=0 --boundary-condition-bottom=0 --output-basepath=$MYDATADIR/swe --output-scale=1"
+	echo Executing: $RCMD
+	srun $RCMD > $MYDATADIR/stdout.log 2>$MYDATADIR/stderr.log
 }
 
 echo Running gcc build
