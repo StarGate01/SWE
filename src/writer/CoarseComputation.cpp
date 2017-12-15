2c230b1edf4acf63f94712a617103146ff4ae920
foxtrot <felix.wieser@tum.de>
2017-12-15 08:15:45 +0100

    Started documentation of CoarseComputation

--- src/writer/CoarseComputation.cpp
+++ src/writer/CoarseComputation.cpp
@@ -7,16 +7,38 @@
 
 using namespace io;
 
+//TODO: Fusion numCellsVertical and numCellsHorzontal
+
+/**
+* @brief Returns the number of rows of the downscaled Float2D object
+* @param ny Number of original vertical cells
+*
+* @return Number of cells in downscaled field
+*/
 int CoarseComputation::numCellsVertical(int ny)
 {
     return (int)ceil(((float)ny)/((float)scale));
 }
 
+/**
+* @brief Returns the number of columns of the downscaled Float2D object
+* @param nx Number of original horizontal cells
+*
+* @return Number of cells in downscaled field
+*/
 int CoarseComputation::numCellsHorizontal(int nx)
 {
     return (int)ceil(((float)nx)/((float)scale));
 }
 
+/**
+* @brief Calculates the average of a subfield within the \param data object from ( \param xmin, \param ymin ) to (\param xmin + scale, \param ymin + scale)
+* @param data Float2D object that is to be downscaled
+* @param xmin Start x index
+* @param ymin Start y index
+*
+* @return Average value of 
+*/
 float CoarseComputation::average(const Float2D& data, int xmin, int ymin)
 {
     float sum = 0;
@@ -26,6 +48,13 @@ float CoarseComputation::average(const Float2D& data, int xmin, int ymin)
     return sum / (scale * scale);
 }
 
+/**
+* @brief Constructor for io::CoarseComputation
+* @param s Scale
+* @param bs BoundarySize object
+* @param ow Original width of the field
+* @param oh Original height of the field
+*/
 CoarseComputation::CoarseComputation(int s, const BoundarySize& bs, int ow, int oh)
     : scale(s), bsize(bs),
       oldWidth(ow), oldHeight(oh),
@@ -34,6 +63,11 @@ CoarseComputation::CoarseComputation(int s, const BoundarySize& bs, int ow, int 
       averages(new Float2D(newWidth, newHeight))
 {}
 
+/**
+* @brief Calculates the average values for the entire field
+*
+* @param data Field to be downscaled
+*/
 void CoarseComputation::updateAverages(const Float2D& data)
 {
     for(int x=0; x<newWidth; x++)
@@ -41,6 +75,9 @@ void CoarseComputation::updateAverages(const Float2D& data)
             (*averages)[x][y] = average(data, scale * x, scale * y);
 }
 
+/**
+* @brief Deconstructor of io::CoarseComputation
+*/
 CoarseComputation::~CoarseComputation()
 {
     delete averages;
--- src/writer/CoarseComputation.hh
+++ src/writer/CoarseComputation.hh
@@ -7,7 +7,7 @@
 #define COARSE_COMPUTATION_HH
 
 #include <cassert>
-#include <cmath>
+#include <cmath>                //Standard math
 #include "tools/help.hh"        //Float1D, Float2D
 #include "Writer.hh"            //BoundarySize
 
@@ -25,10 +25,30 @@ namespace io
 
             BoundarySize bsize;
 
+            /**
+            * @brief Returns the number of rows of the downscaled Float2D object
+            * @param ny Number of original vertical cells
+            *
+            * @return Number of cells in downscaled field
+            */
             int numCellsVertical(int ny);
 
+            /**
+            * @brief Returns the number of columns of the downscaled Float2D object
+            * @param nx Number of original horizontal cells
+            *
+            * @return Number of cells in downscaled field
+            */
             int numCellsHorizontal(int nx);
 
+            /**
+            * @brief Calculates the average of a subfield within the \param data object from ( \param xmin, \param ymin ) to (\param xmin + scale, \param ymin + scale)
+            * @param data Float2D object that is to be downscaled
+            * @param xmin Start x index
+            * @param ymin Start y index
+            *
+            * @return Average value of 
+            */
             float average(const Float2D& data, int xmin, int ymin);
 
         public: 
@@ -36,14 +56,27 @@ namespace io
             int oldWidth, oldHeight, newWidth, newHeight;
             Float2D* averages;
 
+            /**
+            * @brief Constructor for io::CoarseComputation
+            * @param s Scale
+            * @param bs BoundarySize object
+            * @param ow Original width of the field
+            * @param oh Original height of the field
+            */
             CoarseComputation(int s, const BoundarySize& bs, int ow, int oh);
 
+            /**
+            * @brief Calculates the average values for the entire field
+            *
+            * @param data Field to be downscaled
+            */
             void updateAverages(const Float2D& data);
 
+            /**
+            * @brief Deconstructor of io::CoarseComputation
+            */
             ~CoarseComputation();
-
     };
-
 }
 
 #endif
