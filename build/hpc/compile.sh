61a687a5a80944d8a78281883664b37110cc51a1
LRZ-SIM Funktionskennung <di57yec@mpp2-login6.cos.lrz.de>
2017-12-15 10:32:04 +0100

    add debug binary for vtune

--- build/hpc/compile.sh
+++ build/hpc/compile.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-MODE=release
 NCDFDIR=/lrz/sys/libraries/netcdf/4.2.1.1
 
 cd ../..
-scons buildVariablesFile=build/options/SWE_gnu.py compileMode=$MODE netCDFDir=$NCDFDIR
-scons buildVariablesFile=build/options/SWE_intel.py compileMode=$MODE netCDFDir=$NCDFDIR
+scons buildVariablesFile=build/options/SWE_gnu.py compileMode=release netCDFDir=$NCDFDIR
+scons buildVariablesFile=build/options/SWE_intel.py compileMode=release netCDFDir=$NCDFDIR
+scons buildVariablesFile=build/options/SWE_intel.py compileMode=debug netCDFDir=$NCDFDIR
 
