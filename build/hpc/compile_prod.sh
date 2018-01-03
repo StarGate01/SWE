0a94960fe58320110e36b78a8f059a7bf09f6302
Christoph Honal <christoph.honal@web.de>
2018-01-03 13:08:27 +0100

    add fast compile command to prod

--- build/hpc/compile_prod.sh
+++ build/hpc/compile_prod.sh
@@ -4,3 +4,4 @@ NCDFDIR=/lrz/sys/libraries/netcdf/4.2.1.1
 
 cd ../..
 scons buildVariablesFile=build/options/SWE_intel_prod.py compileMode=release netCDFDir=$NCDFDIR
+scons buildVariablesFile=build/options/SWE_intel_fast.py compileMode=release netCDFDir=$NCDFDIR
